5e3ffded4a013adeb71551e9f6fd44d4d1a2a74d
samblake00 <samuel.blake@ucdenver.edu>
2023-06-05 16:07:19 -0600

    changes to weather app

--- weatherapp.py
+++ weatherapp.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import requests
-import json
 from datetime import datetime
 
 # Initialize Window
@@ -11,20 +10,16 @@ root.resizable(0, 0)  # to make the window size fixed
 # title of our window
 root.title("Weather App - Created by Sam")
 
-# ----------------------Functions to fetch and display weather info
 city_value = StringVar()
 
-
+# -------------------Functions to fetch and display weather info------------------
 def time_format_for_location(utc_with_tz):
     local_time = datetime.utcfromtimestamp(utc_with_tz)
     return local_time.time()
 
 
-city_value = StringVar()
-
-
 def showWeather():
-    # Enter you api key, copies from the OpenWeatherMap dashboard
+    # Enter your api key, copies from the OpenWeatherMap dashboard
     api_key = ""  # sample API
 
     # Get city name from user from the input field (later in the code)
@@ -39,16 +34,13 @@ def showWeather():
     # changing response from json to python readable
     weather_info = response.json()
 
-    tfield.delete("1.0", "end")  # to clear the text field for every new output
+    # to clear the text field for every new output
+    tfield.delete("1.0", "end")
 
-    # as per API documentation, if the cod is 200, it means that weather data was successfully fetched
-
+    # 200 means that weather data was successfully fetched
     if weather_info['cod'] == 200:
         kelvin = 273  # value of kelvin
-
-        # -----------Storing the fetched values of weather of a city
-
-        temp = int(weather_info['main']['temp'] - kelvin)  # converting default kelvin value to Celcius
+        temp = int(weather_info['main']['temp'] - kelvin)  # converting default kelvin value to Celsius
         feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
         pressure = weather_info['main']['pressure']
         humidity = weather_info['main']['humidity']
@@ -58,30 +50,24 @@ def showWeather():
         timezone = weather_info['timezone']
         cloudy = weather_info['clouds']['all']
         description = weather_info['weather'][0]['description']
-
         sunrise_time = time_format_for_location(sunrise + timezone)
         sunset_time = time_format_for_location(sunset + timezone)
-
-        # assigning Values to our weather varaible, to display as output
-
+        # assigning Values to our weather variable, to display as output
         weather = f"\nWeather of: {city_name}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {description}"
     else:
-        weather = f"\n\tWeather for '{city_name}' not found!\n\tKindly Enter valid City Name !!"
+        weather = f"\n\tWeather for '{city_name}' not found!\n\t Please enter a valid city name."
 
     tfield.insert(INSERT, weather)  # to insert or send value in our Text Field to display output
 
 
-# ------------------------------Frontend part of code - Interface
+# -------------------Front End Interface-----------------
 
-
-city_head = Label(root, text='Enter City Name', font='Arial 12 bold').pack(pady=10)  # to generate label heading
+city_head = Label(root, text='Enter City Name', font='Arial 12 bold').pack(pady=10)
 
 inp_city = Entry(root, textvariable=city_value, width=24, font='Arial 14 bold').pack()
 
 Button(root, command=showWeather, text="Check Weather", font="Arial 10", bg='lightblue', fg='black',
        activebackground="teal", padx=5, pady=5).pack(pady=20)
-
-# to show output
 
 weather_now = Label(root, text="The Weather is:", font='arial 12 bold').pack(pady=10)
 
